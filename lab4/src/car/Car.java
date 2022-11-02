5b5f25045428b75a116988a11a76a89ab10225ee
thaintd <114746768+thaintd@users.noreply.github.com>
2022-11-02 10:54:18 +0700

    Add files via upload

---
+++ lab4/src/car/Car.java
@@ -0,0 +1,77 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package car;
+
+/**
+ *
+ * @author ntduy
+ */
+public class Car {
+    private String Colour;
+    private int EnginePower;
+    private boolean Convertible;
+    private boolean parkingBrake;
+    
+    public Car (){
+        Colour="";
+        EnginePower=0;
+        Convertible=false;
+        parkingBrake=false;
+    }
+    public Car(String Colour, int EnginePower, boolean Convertible, boolean parkingBrake){
+        this.Colour = Colour;
+        this.EnginePower=EnginePower;
+        this.Convertible= Convertible;
+        this.parkingBrake= parkingBrake;
+    }
+    public void pressStartButton(){
+        System.out.println("You can press the star button");
+    }
+    public void pressAcceleratorButton(){
+        System.out.println("You can press the accelerator button");
+        System.out.println("Colour:"+ Colour);
+        System.out.println("Engine power:"+EnginePower);
+        System.out.println("Convertible:"+Convertible);
+        System.out.println("parking brake:"+parkingBrake);
+    }
+    public void setColour(String Colour) {
+        this.Colour = Colour;
+    }
+    public String getColour() {
+        return Colour;
+    }
+    public int getEnginePower() {
+        return EnginePower;
+    }
+    public void setEnginePower(int EnginePower) {
+        this.EnginePower = EnginePower;
+    }
+    public boolean isConvertible() {
+        return Convertible;
+    }
+    public void setConvertible(boolean Convertible) {
+        this.Convertible = Convertible;
+    }
+    public boolean isParkingBrake() {
+        return parkingBrake;
+    }
+    public void setParkingBrake(boolean parkingBrake) {
+        this.parkingBrake = parkingBrake;
+    }
+    public static void main(String[] args) {
+        Car c=new Car();
+        c.pressStartButton();
+        c.pressAcceleratorButton();
+        
+        Car c2=new Car();
+        c2.pressAcceleratorButton();
+        
+        Car c3=new Car("red", 100, true, true);
+        c3.pressAcceleratorButton();
+        c3.setColour("black");
+        System.out.println("Colour of c3:" + c3.getColour());
+    }
+    
+}
